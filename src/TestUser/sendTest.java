3d51a1c2a842937972f915a1b1cf1e74a283e1c8
willemrenders <willem.renders@student.uantwerpen.be>
2015-12-08 12:12:01 +0100

    Sendtest

---
+++ src/TestUser/sendTest.java
@@ -0,0 +1,35 @@
+package TestUser;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class sendTest
+{
+	private int outPort;
+	private String ip, message;
+	public static void main(String[] args)
+	{
+		try {
+			String sentence;
+			Socket sendsocket = new Socket("localhost", 6789);
+			//outToServer leest uitgaande data op clientSocket
+			DataOutputStream outToServer = new DataOutputStream(sendsocket.getOutputStream());
+			
+			//Input
+			sentence = "<Frederik> Yoooo";
+			outToServer.writeBytes(sentence + '\n');
+			System.out.println("Sent to server: " + sentence);
+			sendsocket.close();
+				
+			}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("<CLIENT> Error");
+		}					
+	}
+
+}
